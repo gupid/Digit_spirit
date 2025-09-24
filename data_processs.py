32f130ecb3e346ef41f1bfc197e646e02dfe075a
gupid <ywq2640780145@163.com>
2025-09-24 21:14:07 +0800

    2025.9.24晚

--- data_processs.py
+++ data_processs.py
@@ -15,16 +15,22 @@ file_paths = [
     #'train_data/system_log_9_9.csv',
     #'train_data/system_log_9_13.csv',
     'train_data/system_log_9_22.csv',
-    'train_data/system_log_9_23.csv',
+    #'train_data/system_log_9_23.csv',
+    'train_data/system_log_9_24.csv',
 ]
 
+file_paths_test = [
+    'test_data/system_log_9_23.csv',
+]
 # 2. 使用列表推导式和循环来读取和处理所有文件
 processed_dfs = [process_dataframe(pd.read_csv(file)) for file in file_paths]
 
+processed_df_test = [process_dataframe(pd.read_csv(file) )for file in file_paths_test]
+
 # 3. 合并所有处理好的DataFrame
 combined_df = pd.concat(processed_dfs, ignore_index=True)
-
+combined_df_test = pd.concat(processed_df_test, ignore_index=True)
 # 4. 保存结果
 combined_df.to_csv('system_log.csv', index=False)
-
-print("数据处理完成，并已保存到 system_log.csv")
+combined_df_test.to_csv('system_log_test.csv', index=False)
+print("数据处理完成，并已保存到 system_log.csv/system_log_test.csv")
